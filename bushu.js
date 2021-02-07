c16533b4e8ad9889259f7f44676d6b80e2db1443
liz <58138651+895527040@users.noreply.github.com>
2021-02-07 22:47:07 +0800

    Update bushu.js

--- bushu.js
+++ bushu.js
@@ -9,17 +9,6 @@ if(!port){
 }
 
 var server = http.createServer(function(request, response){
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var port = process.argv[2]
-
-if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
-  process.exit(1)
-}
-
-var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
   var pathWithQuery = request.url 
   var queryString = ''
@@ -40,7 +29,7 @@ var server = http.createServer(function(request, response){
   response.setHeader('Content-Type', 'text/html;charset=utf-8')
   response.write(`
     <link rel="stylesheet" href="./style.css">
-    <h1>H1标签</h1>
+    <h1>标签是红色的</h1>
   `)
   response.end()
 } else if(path === '/style.css'){
@@ -54,6 +43,27 @@ var server = http.createServer(function(request, response){
       response.write(`你访问的页面不存在`)
   response.end()
 }
+// if(path === '/' ){
+//     console.log("有人访问/了")
+//     response.write(`这就是响应\n`)
+//     response.end()
+// }
+//   if(path === '/'){
+//     response.statusCode = 200
+//     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+//     response.write(`二哈`)
+//     response.end()
+//   } else if(path === '/x'){
+//     response.statusCode = 200
+//     response.setHeader('Content-Type', 'text/css;charset=utf-8')
+//     response.write(`body{color: red;}`)
+//     response.end()
+//   } else {
+//     response.statusCode = 404
+//     response.setHeader('Content-Type', 'text/html;charset=utf-8')
+//     response.write(`你输入的路径不存在对应的内容`)
+//     response.end()
+//   }
 
   /******** 代码结束，下面不要看 ************/
 })
